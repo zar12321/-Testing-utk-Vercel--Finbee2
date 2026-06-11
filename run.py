a74dc6a79251873841e2a061327ab8b3cf410e5d
zar12321 <ldruszardii@gmail.com>
2026-06-11 15:24:14 +0700

    nyoba sebelum pulang

--- run.py
+++ run.py
@@ -1,77 +1,121 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from fastapi import Form
 
 app = FastAPI()
 
 
 @app.get("/", response_class=HTMLResponse)
 def login_page():
+
     return """
-    <!DOCTYPE html>
     <html>
     <head>
         <title>FinBee Login</title>
+
         <style>
-            body{
-                background:#0f172a;
-                color:white;
-                font-family:Arial;
-                display:flex;
-                justify-content:center;
-                align-items:center;
-                height:100vh;
-            }
 
-            .card{
-                width:400px;
-                padding:30px;
-                border-radius:20px;
-                background:#1e293b;
-            }
+        body{
+            background:#0f172a;
+            color:white;
+            font-family:Arial;
+            display:flex;
+            justify-content:center;
+            align-items:center;
+            height:100vh;
+        }
 
-            input{
-                width:100%;
-                padding:12px;
-                margin-top:10px;
-                margin-bottom:20px;
-                border:none;
-                border-radius:10px;
-            }
+        .card{
+            width:400px;
+            background:#1e293b;
+            padding:30px;
+            border-radius:20px;
+        }
 
-            button{
-                width:100%;
-                padding:12px;
-                border:none;
-                border-radius:10px;
-                background:linear-gradient(
-                    90deg,
-                    #f59e0b,
-                    #fbbf24
-                );
-                font-weight:bold;
-            }
+        input{
+            width:100%;
+            padding:12px;
+            margin-top:10px;
+            margin-bottom:20px;
+            border:none;
+            border-radius:10px;
+        }
+
+        button{
+            width:100%;
+            padding:12px;
+            border:none;
+            border-radius:10px;
+            font-weight:bold;
+            background:#fbbf24;
+        }
+
         </style>
+
     </head>
+
     <body>
 
         <div class="card">
-            <h1>🐝 FinBee</h1>
 
-            <input
-                placeholder="Email atau Username"
-            >
+            <h1>🐝 FinBee Login</h1>
 
-            <input
-                type="password"
-                placeholder="Password"
-            >
+            <form action="/login" method="post">
 
-            <button>
-                Masuk
-            </button>
+                <input
+                    name="username"
+                    placeholder="Username"
+                >
+
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                >
+
+                <button type="submit">
+                    Masuk
+                </button>
+
+            </form>
 
         </div>
 
     </body>
     </html>
+    """
+
+@app.post("/login", response_class=HTMLResponse)
+def login(
+    username: str = Form(...),
+    password: str = Form(...)
+):
+
+    if (
+        username == "admin"
+        and
+        password == "123456"
+    ):
+        return """
+        <h1>
+            Login Berhasil 🎉
+        </h1>
+
+        <p>
+            Selamat datang Admin
+        </p>
+        """
+
+    return """
+    <h1>
+        Login Gagal ❌
+    </h1>
+
+    <p>
+        Username atau Password salah
+    </p>
+
+    <a href="/">
+        Kembali ke Login
+    </a>
     """
